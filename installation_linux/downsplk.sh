83d0e982b59cb917dfc346c2e1e542efe99d487d
Rafael Santos <rafamss@gmail.com>
2018-05-09 18:43:41 +0000

    Added md5sum validation

--- installation_linux/downsplk.sh
+++ installation_linux/downsplk.sh
@@ -3,23 +3,32 @@
 # Start the checklist to download Splunk Enterprise
 cd ~/
 mkdir -p downloads && cd $_
-
-echo "###################################################################"
-echo "################## validate if wget is installed ##################"
-echo "###################################################################"
+echo -e "################## validating if wget is installed ##################\n"
 
 if [ ! -x /usr/bin/wget ] ; then
 	sudo yum install wget -y
 else
-	echo "###################################################################"
-	echo "################## wget is installed ##################"
-	echo "###################################################################"
+	echo -e "################## wget is installed ##################\n"
 fi
 
 # Remote download of Splunk Enterprise from the specified repository
-wget -O splunk-7.1.0-2e75b3406c5b-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.0&product=splunk&filename=splunk-7.1.0-2e75b3406c5b-Linux-x86_64.tgz&wget=true'
+wget -O splunkenterprise 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.0&product=splunk&filename=splunk-7.1.0-2e75b3406c5b-Linux-x86_64.tgz&wget=true'
 
 # Remote download from the specified repository of md5 validation file
-wget https://download.splunk.com/products/splunk/releases/7.1.0/linux/splunk-7.1.0-2e75b3406c5b-Linux-x86_64.tgz.md5
+wget -O splunkenterprisemd5 https://download.splunk.com/products/splunk/releases/7.1.0/linux/splunk-7.1.0-2e75b3406c5b-Linux-x86_64.tgz.md5
+
+#validate downloaded files of Splunk Enterprise
+
+md5sum splunkenterprise splunkenterprisemd5 > splunkvalidatemd5
+
+if md5sum --status -c splunkvalidatemd5 ; then
+	# The MD5 sum matched
+		echo -e "splunkenterprise md5 matched\n"
+else
+	# The MD5 sum didn't match
+		echo -e "splunkenterprise md5 didn't match\n"
+fi
+
+echo -e "splunk enterprise installed\n"
 
 exit
