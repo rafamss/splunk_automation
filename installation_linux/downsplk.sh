6e6bdc94f28d77817b36de152861071836bf92a5
Rafael Santos <rafamss@gmail.com>
2018-05-09 17:14:10 +0000

    Correction of showed message

--- installation_linux/downsplk.sh
+++ installation_linux/downsplk.sh
@@ -3,16 +3,23 @@
 # Start the checklist to download Splunk Enterprise
 cd ~/
 mkdir -p downloads && cd $_
+
+echo "###################################################################"
+echo "################## validate if wget is installed ##################"
+echo "###################################################################"
+
 if [ ! -x /usr/bin/wget ] ; then
-   sudo yum install wget -y
+	sudo yum install wget -y
 else
-   echo "################## wget encontra-se instalado previamente.##################"
+	echo "###################################################################"
+	echo "################## wget is installed ##################"
+	echo "###################################################################"
 fi
 
-# Baixar do repositório remoto
+# Remote download of Splunk Enterprise from the specified repository
 wget -O splunk-7.1.0-2e75b3406c5b-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.0&product=splunk&filename=splunk-7.1.0-2e75b3406c5b-Linux-x86_64.tgz&wget=true'
 
-# baixar o md5 para validar o download
+# Remote download from the specified repository of md5 validation file
 wget https://download.splunk.com/products/splunk/releases/7.1.0/linux/splunk-7.1.0-2e75b3406c5b-Linux-x86_64.tgz.md5
 
 exit
