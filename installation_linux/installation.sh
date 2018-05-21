1d8af1e0bea4cebd430d100bf97a08a04a6be097
Rafael Santos <rafamss@gmail.com>
2018-05-21 22:26:32 +0000

    Fixed Installation File

--- installation_linux/installation.sh
+++ installation_linux/installation.sh
@@ -13,8 +13,11 @@ sudo tar -zxvf splunkenterprise.tgz -C /opt
 # change the owner of installation directory
 sudo chown -R $user:$group /opt/splunk
 
+# setting the Splunk Home
+export SPLUNK_HOME='/opt/splunk'
+
 # puting the user credentials at the user-seed.conf
-cd /opt/splunk/etc/system/local
+cd $SPLUNK_HOME/etc/system/local
 touch user-seed.conf
 echo [user_info] > user-seed.conf
 echo USERNAME=admin >> user-seed.conf
@@ -27,6 +30,3 @@ cd $SPLUNK_HOME/bin
 
 # put the splunk in boot start
 ./splunk enable boot-start
-
-# setting the Splunk Home
-export SPLUNK_HOME='/opt/splunk'
