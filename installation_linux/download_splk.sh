81d5e64d61a899a962cf323a58134bb4bc31dc0f
Rafael Martins <rafamss@gmail.com>
2018-11-03 14:11:23 -0300

    Update the Splunk URLs version

--- installation_linux/download_splk.sh
+++ installation_linux/download_splk.sh
@@ -10,8 +10,11 @@
 
 ## Variables definition ##
 ## Set of URL download ###
-URL_download='https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.1&product=splunk&filename=splunk-7.1.1-8f0ead9ec3db-Linux-x86_64.tgz&wget=true'
-URL_md5validation='https://download.splunk.com/products/splunk/releases/7.1.1/linux/splunk-7.1.1-8f0ead9ec3db-Linux-x86_64.tgz.md5'
+## Splunk Enterprise tgz version 7.1.1 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.1&product=splunk&filename=splunk-7.1.1-8f0ead9ec3db-Linux-x86_64.tgz&wget=true'
+## MD5sum Splunk Enterprise version 7.1.1 'https://download.splunk.com/products/splunk/releases/7.1.1/linux/splunk-7.1.1-8f0ead9ec3db-Linux-x86_64.tgz.md5'
+
+URL_download='https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.2.0&product=splunk&filename=splunk-7.2.0-8c86330ac18-Linux-x86_64.tgz&wget=true'
+URL_md5validation="https://download.splunk.com/products/splunk/releases/7.2.0/linux/splunk-7.2.0-8c86330ac18-Linux-x86_64.tgz.md5'
 
 ## Set directory to downdload splunk and validate then ##
 ## Change this if you want to ##
